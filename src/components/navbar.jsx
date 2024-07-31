b3a8bf19795842568764b9b3625ae9103c0cf59d
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-08-01 02:26:36 +0530

    adding the home ui

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import NaacLogo from "../assets/images/naac_logo.png";
+import nirfLogo from "../assets/images/nirf_logo.png";
+import ManuuLogo from "../assets/images/manuu_logo.png";
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="Navbar flex flex-col m-0 p-0 w-full items-center justify-center">
+      <div className="header-wrapper flex flex-col w-11/12 m-0 p-0">
+        <div className="header flex w-full m-0 p-0 items-center justify-between">
+          <img src={NaacLogo} alt="" className="h-[90px] w-[120px]" />
+          <img src={ManuuLogo} alt="" className="h-[90px] w-[320px]" />
+          <img src={nirfLogo} alt="" className="h-[70px] w-[100px]" />
+        </div>
+      </div>
+      <div className="navbar-wrapper flex flex-col w-full m-0 p-0 items-center justify-center bg-tertiary mt-5">
+        <div className="navbar-content w-11/12  flex flex-row ">
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="bg-slate-500 p-3 px-4 text-white">Home</Link>
+          </div>
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="p-3 px-4 text-white">Important Dates</Link>
+          </div>
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="p-3 px-4 text-white">Call for Papers</Link>
+          </div>
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="p-3 px-4 text-white">Committees</Link>
+          </div>
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="p-3 px-4 text-white">Author Info</Link>
+          </div>
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="p-3 px-4 text-white">Schedule</Link>
+          </div>
+          <div className="nav-item flex items-center justify-center ">
+            <Link className="p-3 px-4 text-white">Contact Us</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,9 +1,38 @@
-const Home=()=>{
-    return(
-        <>
-            <h1>Home</h1>
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <div className="main-wrapper flex w-full items-center justify-center mt-12">
+      <div className="flex w-11/12 flex-row items-center justify-center">
+        <div className="w-6/12">
+          <h2 className="text-4xl font-semibold text-blue-600">
+            About ICICC-2024
+          </h2>
+          <p className="text-1xl font-medium">
+            We are extremely pleased to welcome you to ICICC-2024
+          </p>
+        </div>
+        <div className="w-6/12">
+          <p className="text-[16px] text-justify">
+            The objective of this Scopus indexed 7th International Conference on
+            Intelligent Computing and Communication (ICICC – 2024) is to present
+            a unified platform for advanced and multi-disciplinary research
+            towards design of smart computing, information systems, electronic
+            systems. The theme focuses on various innovation paradigms in system
+            knowledge, intelligence and sustainability that may be applied to
+            provide realistic solution to variegated problems in society,
+            environment and industries. The scope is also extended towards
+            deployment of emerging computational and knowledge transfer
+            approaches, optimizing solutions in a variety of disciplines of
+            Computer Science and Engineering, Electronics and Communication
+            Engineering, Electrical and Electronics Engineering, Electronics and
+            Instrumentation Engineering. The conference proceedings aim to
+            publish unpublished work pertaining to the topics mentioned below.
+            The conference is scheduled to be held on August, 2024 at CMR
+            Technical Campus, Hyderabad, Telangana, India{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
